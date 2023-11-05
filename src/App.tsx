49cecc24074c3408d3278e6396347ca5502666e2
Tiago Martins <vargas.tiago@hotmail.com>
2023-11-05 11:30:28 -0300

    Added search filter for name and change card width with select

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ interface CardInfosProps {
   id: string;
   name: string;
   card_images: {
+    image_url: string;
     image_url_cropped: string;
   }[];
   type: string;
@@ -22,6 +23,19 @@ interface CardInfosProps {
 function App() {
   const [cards, setCards] = useState([])
   const [selectedCard, setSelectedCard] = useState<CardInfosProps>();
+  const [searchTerm, setSearchTerm] = useState('');
+  const [cardSize, setCardSize] = useState("medium"); // Valor padrão é "medium"
+  
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredCards = (cards as CardInfosProps[]).filter(card => card.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedSize = event.target.value;
+    setCardSize(selectedSize);
+  };
   
   useEffect(() => {
       axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?&startdate=01/01/2000&enddate=08/23/2002&dateregion=tcg_date').then(response => {
@@ -39,13 +53,19 @@ function App() {
 
   return (
     <>
+      <div>
+        <input type="text" onChange={handleSearchChange} />
+        <select name="cardWidth" id="cardWidth" onChange={handleSelectChange}>
+          <option value="small">Pequeno</option>
+          <option value="medium" selected>Médio</option>
+          <option value="large">Grande</option>
+        </select>
+      </div>
       <div className="content">
-        <div className="card-list">
-          {cards && cards.map(card => {
-            return (        
-              <Card card={card} onClick={() => openCardInfo(card)}/>
-            )
-          })}
+        <div className={`card-list ${cardSize}`}>
+          {filteredCards.map(card => (
+            <Card card={card} onClick={() => openCardInfo(card)}/>
+          ))}
         </div>
         {selectedCard && <CardInfos card={selectedCard} />}
       </div>
